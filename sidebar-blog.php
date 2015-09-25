42431f1b7943345818b5ab62ecb4dc68397616d4
fvuong <fvuong91@gmail.com>
2015-09-24 19:53:13 -0700

    remove unnecessary files

--- sidebar-blog.php
+++
@@ -1,7 +0,0 @@
-<div id="sidebar-blog">
- 
- <?php
-	if ( is_active_sidebar( 'sidebar-blog' ) ) : dynamic_sidebar( 'sidebar-blog' ); ?>
-				
-	<?php endif; ?>
-</div>
--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<div id="sidebar">
- 
- <?php
-	if ( is_active_sidebar( 'sidebar' ) ) : dynamic_sidebar( 'sidebar' ); ?>
-				
-	<?php endif; ?>
-</div>
